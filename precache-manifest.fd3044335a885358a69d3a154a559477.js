6f9548d1c9ceb338d2f60bed8b79872cd2208c4b
OMARc63 <abobakr072221@gmail.com>
2023-05-06 12:37:30 +0100

    Updates

---
+++ precache-manifest.fd3044335a885358a69d3a154a559477.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "00ec82c89b03d01b7c13",
+    "url": "/ecommerce-product/static/css/main.66d1510a.chunk.css"
+  },
+  {
+    "revision": "00ec82c89b03d01b7c13",
+    "url": "/ecommerce-product/static/js/main.00ec82c8.chunk.js"
+  },
+  {
+    "revision": "5bff56686b744a6e6287",
+    "url": "/ecommerce-product/static/js/1.5bff5668.chunk.js"
+  },
+  {
+    "revision": "a4684b741f59fac1fde9",
+    "url": "/ecommerce-product/static/js/2.a4684b74.chunk.js"
+  },
+  {
+    "revision": "2d39802a34e84dc7a03e",
+    "url": "/ecommerce-product/static/js/runtime~main.2d39802a.js"
+  },
+  {
+    "revision": "0ac65e8df3cdb3240e82aac50d5b84c5",
+    "url": "/ecommerce-product/static/media/image-product-1.0ac65e8d.jpg"
+  },
+  {
+    "revision": "383d1284c6fd2728846d4d5033882509",
+    "url": "/ecommerce-product/static/media/icon-close.383d1284.svg"
+  },
+  {
+    "revision": "11c68ec66c6da9819bfefe602bbb9e3b",
+    "url": "/ecommerce-product/static/media/icon-previous.11c68ec6.svg"
+  },
+  {
+    "revision": "b02d406c9da0e59c42120e997154fbbb",
+    "url": "/ecommerce-product/static/media/icon-next.b02d406c.svg"
+  },
+  {
+    "revision": "dc5f56d918a4a172b00d9fb5c782f873",
+    "url": "/ecommerce-product/static/media/image-product-2.dc5f56d9.jpg"
+  },
+  {
+    "revision": "d8f0fd258d7833d69417c1cc12d217bb",
+    "url": "/ecommerce-product/static/media/image-product-3.d8f0fd25.jpg"
+  },
+  {
+    "revision": "5e95fcb499ff1a1c449b25f41fc5bf1b",
+    "url": "/ecommerce-product/static/media/image-product-4.5e95fcb4.jpg"
+  },
+  {
+    "revision": "0035f16e8254ffaef52d8039f515581a",
+    "url": "/ecommerce-product/static/media/icon-minus.0035f16e.svg"
+  },
+  {
+    "revision": "d13f859f0bc3a337e36dd0c85251c30c",
+    "url": "/ecommerce-product/static/media/icon-plus.d13f859f.svg"
+  },
+  {
+    "revision": "b0321cb3e6bf6aef8ecc31e32a577687",
+    "url": "/ecommerce-product/static/media/icon-delete.b0321cb3.svg"
+  },
+  {
+    "revision": "06305c96f6e5bbef5501440e19da4d62",
+    "url": "/ecommerce-product/static/media/logo.06305c96.svg"
+  },
+  {
+    "revision": "2166e08d2f67f82606d57886e65f367c",
+    "url": "/ecommerce-product/static/media/icon-menu.2166e08d.svg"
+  },
+  {
+    "revision": "97f12e4073b81172db26996d79d30639",
+    "url": "/ecommerce-product/index.html"
+  }
+];
